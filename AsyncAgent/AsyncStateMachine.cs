a8292f7355ddb9b86315e94ce33839b5f719e2d6
g-un-- <ginoungureanu.mail@gmail.com>
2016-07-31 16:54:27 +0300

    Added AsyncStateMachine class

---
+++ AsyncAgent/AsyncStateMachine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAgentLib
+{
+    public class AsyncStateMachine : IDisposable
+    {
+        private AsyncAgent<Func<CancellationToken, Task>> _processor;
+
+        public AsyncStateMachine()
+        {
+            _processor = new AsyncAgent<Func<CancellationToken, Task>>(async (message, cancellationToken) =>
+            {
+                await message(cancellationToken);
+            });
+        }
+
+        protected void Send<T>(T message, Func<T, CancellationToken, Task> handler)
+        {
+            _processor.Send(async (cancellationToken) =>
+            {
+                await handler(message, cancellationToken);
+            });
+        }
+
+        public void Dispose()
+        {
+            _processor.Dispose();
+        }
+    }
+}
