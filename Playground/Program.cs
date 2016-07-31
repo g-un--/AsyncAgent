5df144539a9143cd4f2716bb6bd3708fb6dda7b9
g-un-- <ginoungureanu.mail@gmail.com>
2016-07-31 16:09:35 +0300

    Small perf test refactoring

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace Playground
 {
@@ -15,58 +16,63 @@ namespace Playground
         {
             var tokenSource = new CancellationTokenSource();
             TestPerformance(tokenSource.Token);
-            Console.ReadLine();
+            ReadLine();
             tokenSource.Cancel();
         }
 
-        private static Task TestPerformance(CancellationToken cancellationToken)
+        private static Task TestPerformance(CancellationToken ct)
         {
-            return Task.Run(() =>
+            return Task.Run(async () =>
             {
                 var stopwatch = new Stopwatch();
-                long sum = 0;
-                int items = 0;
+                
                 int test = 0;
-
-                while (!cancellationToken.IsCancellationRequested)
+                while (!ct.IsCancellationRequested)
                 {
-                    var agent = new AsyncAgent<long>(async (msg, ct) =>
-                    {
-                        sum += msg;
-                        items += 1;
-                        await Task.Delay(0);
-                        if (items == 1000000)
-                        {
-                            stopwatch.Stop();
-                            Console.WriteLine("Items: [1..{0}], Sum: {1}, Time: {2}ms", items, sum, stopwatch.ElapsedMilliseconds);
-                            stopwatch.Reset();
-                            sum = 0;
-                            items = 0;
-                        }
-                    });
-
                     test += 1;
+
+                    var agent = GetNewAgent(stopwatch);
                     stopwatch.Start();
 
                     for (long item = 1; item <= 1000000; item++)
                     {
                         long msg = item;
-
                         agent.Send(msg);
                     }
 
-                    Thread.Sleep(1000);
+                    await Task.Delay(1000, ct);
                     agent.Dispose();
 
                     if (test % 10 == 0)
                     {
                         test = 0;
-                        Console.Clear();
-                        Thread.Sleep(1000);
+                        Clear();
+                        await Task.Delay(1000, ct);
                         stopwatch.Reset();
                     }
                 }
-            }, cancellationToken);
+            }, ct);
+        }
+
+        static AsyncAgent<long> GetNewAgent(Stopwatch stopwatch)
+        {
+            long sum = 0;
+            int items = 0;
+
+            return new AsyncAgent<long>(async (msg, ct) =>
+            {
+                sum += msg;
+                items += 1;
+                await Task.Delay(0);
+                if (items == 1000000)
+                {
+                    stopwatch.Stop();
+                    WriteLine($"Items: [1..{items}], Sum: {sum}, Time: {stopwatch.ElapsedMilliseconds}ms");
+                    stopwatch.Reset();
+                    sum = 0;
+                    items = 0;
+                }
+            });
         }
     }
 }
