826eba2ac5e44aee74980d5c5c3a3ddf037548d0
g-un-- <ginoungureanu.mail@gmail.com>
2016-08-05 18:06:28 +0300

    Check if FAKE is installled

--- build.cmd
+++ build.cmd
@@ -1,8 +1,13 @@
 @echo off
 cd /d %~dp0
 cls
+
 IF NOT EXIST ".\NuGet.exe" (
     powershell "invoke-webrequest https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -outfile NuGet.exe"
 )
-NuGet.exe "Install" "FAKE" "-OutputDirectory" "packages" "-ExcludeVersion"  
+
+IF NOT EXIST ".\packages\FAKE\" (
+    NuGet.exe "Install" "FAKE" "-OutputDirectory" "packages" "-ExcludeVersion"  
+)
+
 "packages\FAKE\tools\Fake.exe" build.fsx %*
